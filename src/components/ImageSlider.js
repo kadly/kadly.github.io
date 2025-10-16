9bf79158d170d1138da66d6e269b1ec15e3f1258
kadly <d111itry@gmail.com>
2025-10-16 19:27:50 +0700

    fix: Добавлена отложенная инициализация карусели для исправления скролла

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import styled from "styled-components";
@@ -9,21 +9,28 @@ import "slick-carousel/slick/slick-theme.css";
 
 const SliderWrapper = styled.div`
   width: 100%;
-  overflow: hidden; /* Финальная страховка от переполнения */
-
-  .slick-slide > div {
-    /* Заставляем контейнер слайда центрировать изображение */
-    display: flex;
-    justify-content: center;
-  }
+  opacity: ${props => (props.visible ? 1 : 0)};
+  transition: opacity 300ms ease-in-out;
 
   .gatsby-image-wrapper {
-    /* Заставляем изображение занимать 100% ширины контейнера */
     width: 100%;
   }
 `;
 
 const ImageSlider = ({ images }) => {
+  const [isSliderVisible, setIsSliderVisible] = useState(false);
+
+  useEffect(() => {
+    // Используем setTimeout с задержкой 0, чтобы этот код выполнился
+    // после того, как основной поток рендеринга завершится.
+    // Это дает карусели время для правильного расчета ширины.
+    const timer = setTimeout(() => {
+      setIsSliderVisible(true);
+    }, 0);
+
+    return () => clearTimeout(timer);
+  }, []); // Пустой массив зависимостей означает, что эффект выполнится один раз
+
   const settings = {
     dots: false,
     arrows: false,
@@ -37,17 +44,19 @@ const ImageSlider = ({ images }) => {
   };
 
   return (
-    <SliderWrapper>
-      <Slider {...settings}>
-        {images.map((imageNode, index) => {
-          const image = getImage(imageNode.node);
-          return (
-            <div key={index}>
-              <GatsbyImage image={image} alt={`Slide ${index + 1}`} />
-            </div>
-          );
-        })}
-      </Slider>
+    <SliderWrapper visible={isSliderVisible}>
+      {isSliderVisible && (
+        <Slider {...settings}>
+          {images.map((imageNode, index) => {
+            const image = getImage(imageNode.node);
+            return (
+              <div key={index}>
+                <GatsbyImage image={image} alt={`Slide ${index + 1}`} />
+              </div>
+            );
+          })}
+        </Slider>
+      )}
     </SliderWrapper>
   );
 };
